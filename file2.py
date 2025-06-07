9c80f7ef64c9dd0ba88ade5c3ef0a21d6711214f
pakkio <claudio.pacchiega@gmail.com>
2025-06-07 19:09:47 +0200

    PAK_CORE SPLITTING

---
+++ file2.py
@@ -0,0 +1,8 @@
+def hello():
+    print("Hello Universe!")  # Modified
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):  # New method
+    return a - b
---
+++ target_file.py
@@ -0,0 +1,5 @@
+def hello():
+    print("Hello World")
+
+def add(a, b):
+    return a + b
